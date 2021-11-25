6b9561661835d2258cb21d34f5ea3374b09c45f0
Sim <vov4.games@yandex.ru>
2021-11-25 19:49:10 +0200

    HW3

--- core/src/com/naumdeveloper/StarGame.java
+++ core/src/com/naumdeveloper/StarGame.java
@@ -1,47 +1,13 @@
-package ru.gb;
+package com.naumdeveloper;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	TextureRegion region;
+import com.naumdeveloper.screen.impl.MenuScreen;
 
-	int x, y;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 30, 40, 100, 150);
-	}
+public class StarGame extends Game {
 
 	@Override
-	public void render () {
-		x+=1; y+=1;
-		ScreenUtils.clear(Color.BROWN);
-		batch.begin();
-		batch.draw(img, x, y);
-		batch.setColor(0.45f, 0.21f, 0.67f, 0.9f);
-		batch.draw(img, 120, 300, 100, 100);
-		batch.setColor(1f, 1f, 1f, 1);
-		batch.draw(region, 250, 400, 100, 150);
-
-//      Текущие размеры экрана
-		Gdx.graphics.getHeight();
-		Gdx.graphics.getWidth();
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		setScreen(new MenuScreen());
 	}
 }
---
+++ core/src/com/naumdeveloper/sprite/impl/Background.java
@@ -0,0 +1,20 @@
+package com.naumdeveloper.sprite.impl;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import com.naumdeveloper.math.Rect;
+import com.naumdeveloper.sprite.Sprite;
+
+public class Background extends Sprite {
+
+    public Background(Texture texture) {
+        super(new TextureRegion(texture));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(worldBounds.getHeight());
+        this.pos.set(worldBounds.pos);
+    }
+}
